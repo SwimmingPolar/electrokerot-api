7536e1dbb0b14e38ac6d8a19b043c455066b34da
SwimmingPolar <53928959+SwimmingPolar@users.noreply.github.com>
2022-07-12 03:39:52 +0900

    Fixed hot reloading and tested

---
+++ src/utils/logger.ts
@@ -0,0 +1,43 @@
+import fs from 'fs'
+
+import { greenBright, yellowBright, cyanBright, redBright } from 'chalk'
+
+/**
+ * @method info - log successful message to console
+ * @method error - log critical error to consoel and exit process with 1
+ */
+const log = {
+  /**
+   * log successful message to console
+   * @param message Message to show
+   */
+  info(info: string): void {
+    console.log(
+      new Date().toISOString() +
+        ' ' +
+        `[${greenBright('INFO')}]` +
+        ': ' +
+        cyanBright(info)
+    )
+  },
+  /**
+   * log critical error to console and exit process
+   * @param invoker Prefix to log file
+   * @param error Error message
+   */
+  error(invoker: string, error: string): void {
+    console.log(
+      new Date().toISOString() +
+        ' ' +
+        `[${yellowBright('ERROR')}]` +
+        ': ' +
+        redBright(error)
+    )
+    fs.appendFileSync(
+      `../log/${invoker}-error-log.txt`,
+      new Date().toISOString() + ' - ' + error + '\n'
+    )
+    process.exit(1)
+  },
+}
+export default log
