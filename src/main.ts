adfcb9c85484025461da91661409c15a3188c42a
YDH <swimmingpolar@gmail.com>
2022-12-27 21:27:20 +0900

    chore:

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory, Reflector } from '@nestjs/core'
+import compression from 'compression'
 import cookieParser from 'cookie-parser'
 import { AppModule } from 'src/app.module'
 import { InvalidJsonFilter } from './common/filters/InvalidJsonFilter'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.enableCors()
-  app.use(cookieParser())
+
   app.setGlobalPrefix('v1')
+
+  app.enableCors()
+  app.use(compression())
+  app.use(cookieParser())
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
